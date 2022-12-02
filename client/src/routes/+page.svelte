afcbf204703d5fedc6ed8e98b53d9e38fea1d733
br-1ght <fcw2k16@gmail.com>
2022-12-02 18:45:59 +0800

    login/sign up page mostly done

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,66 +1,37 @@
 <svelte:head>
-  <title>{"Bubbles"}</title>
+  <title>Bubbles | Login</title>
 </svelte:head>
 
 <script>
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-const passwordRegex = `(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}`
+  const passwordRegex = `(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}`
+  let email = ""
+  let username = ""
+  let password = ""
+  let confirmPassword = ""
+  let requirementsDisplay
+  let lowerCaseFulfill
+  let upperCaseFulfill
+  let numberFulfill
+  let lengthFulfill
 
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {  
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+  const showRequirements = () => {
+    requirementsDisplay = true
   }
   
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {  
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {  
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
+  const hideRequirements = () => {
+    requirementsDisplay = false
   }
   
-  // Validate length
-  if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
+  const checkPasswordRequirement = () => {
+    const lowerCaseRegex = /[a-z]/g
+    const upperCaseRegex = /[A-Z]/g
+    const numberRegex = /[0-9]/g
+    
+    lowerCaseFulfill = Boolean(password.match(lowerCaseRegex))
+    upperCaseFulfill = Boolean(password.match(upperCaseRegex))
+    numberFulfill = Boolean(password.match(numberRegex))
+    lengthFulfill = password.length >= 8
   }
-}
 </script>
 
 <style>
@@ -133,36 +104,56 @@ myInput.onkeyup = function() {
           <form class="">
             <!-- email address -->
             <div class="form-floating mb-3">
-              <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" required>
+              <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" bind:value={email} required>
               <label for="floatingInput">Email address</label>
             </div>
 
             <!-- username -->
             <div class="form-floating mb-3">
-              <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="username" required>
+              <input type="text" class="form-control rounded-3" id="floatingInput" placeholder="username" bind:value={username} required>
               <label for="floatingInput">Username</label>
             </div>
 
             <!-- password -->
             <div class="form-floating mb-3">
-              <input type="password" class="form-control rounded-3" id="psw" name="psw" pattern="{passwordRegex}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" placeholder="Password" required>
+              <input type="password"
+               class="form-control rounded-3"
+               id="psw"
+               name="psw"
+               pattern={passwordRegex} 
+               bind:value={password}
+               on:click={showRequirements}
+               on:keyup={checkPasswordRequirement}
+               on:blur={hideRequirements}
+               title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" 
+               placeholder="Password" 
+               required>
               <label for="floatingPassword">Password</label>
             </div>
             <!-- confirm password -->
             <div class="form-floating mb-3">
-              <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" required>
+              <input type="password"
+              class="form-control rounded-3" 
+              bind:value={confirmPassword}
+              id="floatingPassword" 
+              placeholder="Password" 
+              required>
               <label for="floatingPassword">Confirm password</label>
             </div>
 
             <!-- password reuirements -->
-            <div id="message">
-              <h3>Password must contain the following:</h3>
-              <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-              <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-              <p id="number" class="invalid">A <b>number</b></p>
-              <p id="length" class="invalid">Minimum <b>8 characters</b></p>
-            </div>
-
+            {#if requirementsDisplay}
+              <div id="message">
+                {#if !(lowerCaseFulfill && upperCaseFulfill && numberFulfill && lengthFulfill)}
+                  <h3>Password must contain the following:</h3>
+                {/if}
+                <p id="letter" class="{lowerCaseFulfill === true ? "d-none" : ''}">A <b>lowercase</b> letter</p>
+                <p id="capital" class="{upperCaseFulfill === true ? "d-none" : ''}">A <b>capital (uppercase)</b> letter</p>
+                <p id="number" class="{numberFulfill === true ? "d-none" : ''}">A <b>number</b></p>
+                <p id="length" class="{lengthFulfill === true ? "d-none" : ''}">Minimum <b>8 characters</b></p>
+              </div>
+            {/if}
+            
 
             <button class="login-btn w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
             <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
@@ -172,7 +163,6 @@ myInput.onkeyup = function() {
             <button class="w-100 py-2 mb-2 btn btn-outline-dark rounded-3" type="submit">
               Log in here
             </button>
-            
           </form>
         </div>
       </div>
--- server/app.py
+++ server/app.py
@@ -14,6 +14,7 @@ DEBUG = True
 PORT = 5000
 
 sio = socketio.AsyncServer(async_mode=ASYNC_MODE, cors_allowed_origins=CORS_ALLOWED_ORIGINS)
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 app = Quart(__name__)
 app = cors(app)
 sio_app = socketio.ASGIApp(sio, app)
@@ -28,8 +29,8 @@ app.secret_key = "secret123"
 @sio.event
 async def test(sid, data):
     print(f"Received data: {data}")
-    await sio.emit("response", {"data":f"We received: {data['data']}"}, to=sid)
-    await sio.emit("response", {"data":f"Broadcast: {data['data']}"})
+    await sio.emit("response", {"data": f"We received: {data['data']}"}, to=sid)
+    await sio.emit("response", {"data": f"Broadcast: {data['data']}"})
 
 
 asyncio.run(serve(sio_app, config))
