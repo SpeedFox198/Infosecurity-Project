abe97c2415679e2f91ab8e10990aa048ab06a208
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-12-08 12:54:20 +0800

    Google stuff

---
+++ server/google_authenticator/google_email_send.py
@@ -0,0 +1,44 @@
+import base64
+from google_authenticator import get_service
+from email.message import EmailMessage
+
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def gmail_send(email, subject, content):
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    service = get_service()
+
+    try:
+        message = EmailMessage()
+
+        message.set_content(content)
+
+        message['To'] = email
+        message['From'] = 'bubblesarepretty126@gmail.com'
+        message['Subject'] = subject
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
