be90d038e689a65a0ce80ec552f582e2a8ed0c65
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-12-10 19:02:25 +0800

    Update Nav.svelte

--- client/src/routes/chat/Nav.svelte
+++ client/src/routes/chat/Nav.svelte
@@ -5,30 +5,26 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="image-size">
-      <a href="#Profile Settings"><img class="rounded-circle img-fluid" src="/galaxy.jpg" alt="Me"></a>
-    </div>
-    <span class="px-2 py-2">{currentUser.username}</span>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="##">Home</a>
-        </li>
-      </ul>
-    </div>
+    <ul class="navbar-nav me-auto mb-lg-0 edit">
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      </form>
+    </ul>
   </div>
 </nav>
+
+<footer>  
+</footer>
 
 <style>
   .image-size {
     width: 50px;
     height: 50px;
   }
+
+ .edit {
+   padding-left: 80px;
+   padding-right: 80px;
+
+ }
 </style>
