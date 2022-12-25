2f7a782b276df255389e4df45cee7f3c17c9d9f1
br-1ght <fcw2k16@gmail.com>
2022-12-25 19:22:26 +0800

    better sign up

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -6,22 +6,27 @@
   let signupDisplay = false
   let errors
   
-  if (form?.loginError) {
-    errors = form.loginError
-  }
-  
-  if (form?.cfError) {
-    errors = form.cfError
-  }
-
-  if (form?.signupError) {
-    errors = form.signupError
-  }
-
   const toggleSignupOn = () => {
     signupDisplay = !signupDisplay
   }
 
+  if (form?.loginError) {
+    errors = form.loginError
+  }
+  
+  if (form?.signupError) {
+    errors = form.signupError
+    toggleSignupOn()
+  }
+  
+  if (form?.loginCfError) {
+    errors = form.loginCfError
+  }
+  
+  if (form?.signupCfError) {
+    errors = form.signupCfError
+    toggleSignupOn()
+  }
 </script>
 
 
@@ -39,7 +44,7 @@
       <div class="col-md-6">
         {#if (signupDisplay)}
         <!-- Sign up -->
-        <Signup toggleSignupOn={toggleSignupOn}/>
+        <Signup toggleSignupOn={toggleSignupOn} errors={errors}/>
         {:else}
         <!-- Login -->
         <Login toggleSignupOn={toggleSignupOn} errors={errors} />
