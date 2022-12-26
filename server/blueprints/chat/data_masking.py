b751d46479c14adb3c85d85fff00e7d3ccdcf576
br-1ght <fcw2k16@gmail.com>
2022-12-26 22:53:13 +0800

    comments

--- server/blueprints/chat/data_masking.py
+++ server/blueprints/chat/data_masking.py
@@ -5,7 +5,7 @@ async def mask_sensitive_data(word: str) -> str:
     """
     Detects the following
     Credit cards - Visa, Mastercard, American Express, Diners Club, Discover and JCB
-    NRIC
+    Singapore NRIC
     """
     CREDIT_CARD_NUMBERS_PATTERN = r"(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})"
     NRIC_PATTERN = r"[STst][0-9]{7}[A-Za-z]"
