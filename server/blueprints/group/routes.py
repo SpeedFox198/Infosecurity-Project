6698615e1cc640848d65f373f408b6c8024d33b4
br-1ght <fcw2k16@gmail.com>
2023-01-12 16:45:13 +0800

    comments

--- server/blueprints/group/routes.py
+++ server/blueprints/group/routes.py
@@ -13,10 +13,15 @@ group_bp = Blueprint("group", __name__, url_prefix="/group")
 @login_required
 async def create_group():
     group_photo: FileStorage | None = (await request.files).get("group_photo")
-    metadata: dict = json.loads(
+    """
+    Metadata structure
+    group_name: str
+    disappearing: str (24h, 7d, 30d)
+    users: list[str] (list of user_ids)  
+    """
+    group_metadata: dict = json.loads(
         (await request.form).get("metadata")
     )
-
     # TODO Implement DB logic
     """-- Code Here --"""
     return {"message": "ball"}, 200
