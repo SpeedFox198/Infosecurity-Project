169c807c88b7135703a4cf4fc4651ba13afc6f1d
SpeedFox198 <xstarx125@gmail.com>
2023-01-13 11:29:02 +0800

    updated server.ps1 for uvicorn

--- server.ps1
+++ server.ps1
@@ -1,6 +1,7 @@
 try {
     cd ./server
-    hypercorn app:sio_app --certfile ../cert.pem --keyfile ../key.pem -c config.toml --reload
+    # hypercorn app:sio_app --certfile ../cert.pem --keyfile ../key.pem -c config.toml --reload
+    python ./development.py
 }
 finally {
     cd ..
