7a3da3bb27a8fdc9a4ec74f10c30817e6c4b3b4b
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2023-01-16 09:54:45 +0800

    2FA WIP

--- server/blueprints/settings/routes.py
+++ server/blueprints/settings/routes.py
@@ -17,6 +17,18 @@ from db_access.backup_codes import create_2fa_backup_codes
 settings_bp = Blueprint("settings", __name__, url_prefix="/settings")
 
 
+@settings_bp.post("/twofa_secretgenerate")
+@login_required
+async def google_secret():
+    #Generate the secret token
+    twoFA_checker = await check_2fa_exists(await current_user.user_id)
+    if twoFA_checker:
+        secret_token = pyotp.random_base32()
+        await create_2fa(await current_user.user_id, secret_token)
+        return {"message":"Secret generated"}, 200
+    else:
+        return {"message": "Secret Already Exists"}, 200
+
 #Set 2FA user page
 @settings_bp.post("/twofa")
 @login_required
@@ -27,35 +39,20 @@ async def google_authenticator(data: TwoFABody):
     if twoFA_checker:
         return {"message": "2FA already enabled"}, 400
     else:
-        #Create 2FA after checking if user input is valid
-        secret_token = pyotp.random_base32()
+        twoFA_code = (await get_2fa(await current_user.user_id))[1]
+        secret_token = twoFA_code
         OTP_check = data.twofacode
         verified = pyotp.TOTP(secret_token).verify(OTP_check)
         if verified:
-            await create_2fa(await current_user.user_id, secret_token)
             return {"message": "2FA enabled"}, 200
         else:
             return {"message": "Invalid 2FA code"}, 400
-
-@settings_bp.get("/twofa_secretgenerate")
-@login_required
-async def google_secret():
-    #Generate the secret token
-    twoFA_checker = await check_2fa_exists(await current_user.user_id)
-    if twoFA_checker:
-        secret_token = pyotp.random_base32()
-        await create_2fa(await current_user.userid, secret_token)
-        return {"message":"Secret generated"}, 200
-    else:
-        return {"message": "Secret Already Exists"}, 200
-
 
 @settings_bp.get("/twofa_backupcode")
 @login_required
 async def google_authenticator_backupcodes():
     #Check if 2FA is already enabled
-    twoFA_checker = await check_2fa_exists(await current_user.user_id)
-    if twoFA_checker:
+    if current_user.twofa_status:
         #Check for existing backup codes
         backupcode_checker = await get_2fa_backup_codes(await current_user.user_id)
         if backupcode_checker:
@@ -68,11 +65,10 @@ async def google_authenticator_backupcodes():
         return {"message": "2FA not enabled"}, 400
 
 
-@settings_bp.delete("/twofa")
+@settings_bp.delete("/twofa-delete")
 @login_required
 async def delete_2fa():
-    twoFA_checker = await check_2fa_exists(await current_user.user_id)
-    if twoFA_checker:
+    if current_user.twofa_status:
         await delete_2fa(await current_user.user_id)
         return {"message": "2FA disabled"}, 200
     else:
