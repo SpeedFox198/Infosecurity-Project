f9210e726998c45b458c5e13cd02150e73829186
br-1ght <fcw2k16@gmail.com>
2023-01-19 13:14:23 +0800

    more friends

---
+++ client/src/lib/friends/Friend.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let friend
+</script>
+
+<div class="friend d-flex py-2 px-3 user-select-none align-items-center">
+	<div class="icon">
+		<div class="img-wrapper img-1-1">
+			<img class="rounded-circle" src={friend.avatar} alt="{friend.username} avatar" />
+		</div>
+	</div>
+	<div class="d-flex align-items-center">
+		<span>{friend.username}</span>
+	</div>
+  <slot></slot>
+</div>
+
+<style>
+.friend {
+  border-bottom: 0.1rem solid var(--primary-light-shadow);
+}
+
+.friend:hover {
+  cursor: pointer;
+  background-color: var(--primary-light-shadow);
+}
+
+.icon {
+  height: 4.5rem;
+  width: 4.5rem;
+}
+</style>
--- server/models/response_data/__init__.py
+++ server/models/response_data/__init__.py
@@ -1,3 +1,4 @@
 from .DeviceListData import DeviceListData, DeviceData
 from .UserData import UserData
 from .FriendData import FriendData
+from .FriendRequestsData import FriendRequestsData
