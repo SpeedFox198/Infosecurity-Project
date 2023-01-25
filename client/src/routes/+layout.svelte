14fb534c4222e51298c447f4f0f79a3c2d829ff0
br-1ght <fcw2k16@gmail.com>
2023-01-26 01:05:27 +0800

    Update +layout.svelte

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -13,11 +13,18 @@
     setTimeout(removeFlash, flashTimeout)
   }
   
+  const alertType = {
+    "success": "success",
+    "failure": "danger",
+    "warning": "warning"
+
+  }
+  
 </script>
 
 {#if $flash}
   <!-- Flash messages-->
-  <div class="alert alert-dismissible flash fade show alert-{$flash.type}" role="alert">
+  <div class="alert alert-dismissible flash fade show alert-{alertType[$flash.type]}" role="alert">
     {$flash.message}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={removeFlash}></button>
   </div>
