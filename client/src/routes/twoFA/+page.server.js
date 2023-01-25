ff9824a637c56fa5a5aa3d7ed719843802528f88
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2023-01-26 01:54:04 +0800

    2FA done

--- client/src/routes/twoFA/+page.server.js
+++ client/src/routes/twoFA/+page.server.js
@@ -1,6 +1,12 @@
 //import { redirect } from '@sveltejs/kit'
 import { redirect } from "sveltekit-flash-message/server"
 
+
+import tough from "tough-cookie"
+
+
+const Cookie = tough.Cookie
+
 export async function load({ cookies }) {
   if (!cookies.get("session")) {
     throw redirect(302, "/")
@@ -29,6 +35,14 @@ export const actions = {
             }
         }
       
+        const quartCookie = Cookie.parse(response.headers.get("set-cookie"))
+        event.cookies.set(quartCookie.key, quartCookie.value, {
+          path: quartCookie.path,
+          httpOnly: quartCookie.httpOnly,
+          sameSite: quartCookie.sameSite,
+          maxAge: 60 * 24 * 60 * 60, // formatted as (days * hours * minutes * seconds)
+        })
+
         //throw redirect(302, "/")
         throw redirect(
           302,
