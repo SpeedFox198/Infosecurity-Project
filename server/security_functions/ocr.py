eb79da4d84cf490380e405ae0cec7f71846ba259
br-1ght <fcw2k16@gmail.com>
2023-01-30 10:19:07 +0800

    make python ocr same as js

--- server/security_functions/ocr.py
+++ server/security_functions/ocr.py
@@ -1,10 +1,12 @@
 import cv2
 import pytesseract as pt
 import re
+import string
 
 pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-pt_config = "--psm 6"
+whitelisted_characters = string.ascii_letters + string.digits + " -"
+pt_config = fr"-c tessedit_char_whitelist={whitelisted_characters} --psm 6"
 
 CREDIT_CARD_PATTERN = r'(?:4[0-9]{3}[ -]?[0-9]{4}[ -]?[0-9]{4}[ -]?[0-9]{4}|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{3}[ -]?[0-9]{4}[ -]?[0-9]{4}[ -]?[0-9]{4}|3[47][0-9]{3}[ -]?[0-9]{6}[ -]?[0-9]{5}|3(?:0[0-5]|[68][0-9])[0-9]{3}[ -]?[0-9]{6}[ -]?[0-9]{4}|6(?:011|5[0-9]{2})[0-9]{3}[ -]?[0-9]{4}[ -]?[0-9]{4}[ -]?[0-9]{4}|(?:2131|1800|35\d{3})\d{3}[ -]?[0-9]{4}[ -]?[0-9]{4}[ -]?[0-9]{4})'
 NRIC_PATTERN = r'[STst][0-9]{7}[A-Za-z]'
