69b620ce3d48ad74101570d8a6185599d0697a02
br-1ght <fcw2k16@gmail.com>
2023-02-02 02:07:01 +0800

    yes

--- server/blueprints/settings/routes.py
+++ server/blueprints/settings/routes.py
@@ -90,6 +90,17 @@ async def get_account_information():
     async with async_session() as session:
         # get user details
         user_results = await get_user_details(await current_user.user_id)
+        user_results_dict = {
+            "username": user_results[0],
+            "email": user_results[1],
+            "avatar": user_results[2],
+            "dark_mode": user_results[4],
+            "malware_scan": user_results[5],
+            "friends_only": user_results[6],
+            "censor": user_results[7],
+            "google_account": user_results[8],
+            "disappearing_messages": user_results[9]
+        }
 
         # get device details
         statement = sa.select(Device) \
@@ -111,17 +122,19 @@ async def get_account_information():
             )
 
     # convert all data into json format
-    user_json = json.dumps(user_results, indent=4, default=str)
+    user_json = json.dumps(user_results_dict, indent=4, default=str)
     device_json = json.dumps(device_json_list, indent=4, default=str)
 
     export_path = f"media/exports/{await current_user.user_id}"
     os.makedirs(export_path, exist_ok=True)
     # write to json file
     with open(export_path + "/account_data.json", "w") as outfile:
-        outfile.write(user_json + "\n")
-
-    with open(export_path + "/account_data.json", "a") as outfile:
+        outfile.write("Your account information\n")
+        outfile.write(user_json + "\n\n")
+        outfile.write("Your device information\n")
         outfile.write(device_json)
+
+    # with open(export_path + "/account_data.json", "a") as outfile:
 
     # write to html file
     html_template = await render_template("account_data.html",
