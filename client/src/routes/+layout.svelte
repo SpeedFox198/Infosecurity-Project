49d3d6b35c0c2201f839a900d61e14c76a2694b2
br-1ght <fcw2k16@gmail.com>
2023-02-01 09:01:57 +0800

    make notification as component

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { fly, fade } from 'svelte/transition';
   import { initFlash } from "sveltekit-flash-message/client"
   import { page } from "$app/stores"
+	import Toast from '$lib/general/Toast.svelte';
   
   const flash = initFlash(page)
   const flashTimeout = 5000
@@ -28,54 +28,11 @@
 </script>
 
 {#if $flash}
-  <div class="b-toast p-3 d-flex flex-column" in:fly="{{ y: -200, duration: 1000 }}" out:fade role="alert">
-    <div class="{alertClass[$flash.type]} p-2 header">
-      <div class="d-flex">
-        <span class="flex-grow-1 align-items-center">{alertType[$flash.type]}</span>
-        <button type="button" on:click={removeFlash} class="remove-btn">
-          <i class="fa-solid fa-xmark"></i>
-        </button>
-      </div>
-    </div>
-    <div>
-      {$flash.message}
-    </div>
-  </div> 
+  <Toast 
+    alertClass={alertClass[$flash.type]}
+    alertType={alertType[$flash.type]}
+    message={$flash.message}
+    on:click={removeFlash}
+  />
 {/if}
 <slot />
-
-<style>
-  .b-toast {
-    width: 400px;
-    max-width: 100%;
-    background-color: rgba(255, 255, 255, .85);
-    border-radius: 0.5rem;
-    position: fixed;
-    left: 50%;
-    top: 2rem;
-    z-index: 5;
-  }
-  
-  .header {
-    color: var(--white);
-    border-radius: 0.5rem;
-  }
-  
-  .pos {
-    background-color: #39ff14;
-  } 
-  
-  .neg {
-    background-color: #E10600;
-  }
-  
-  .warn {
-    background-color: yellow;
-  }
-  
-  .remove-btn {
-    border: 0;
-    color: inherit;
-    background-color: inherit;
-  }
-</style>
