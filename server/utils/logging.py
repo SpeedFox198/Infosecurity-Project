22308fc5a51b1ff262247c08024c99e50cb5adf1
br-1ght <fcw2k16@gmail.com>
2023-02-07 19:22:36 +0800

    logging supercharged

--- server/utils/logging.py
+++ server/utils/logging.py
@@ -1,21 +1,30 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler, SysLogHandler
 from datetime import datetime
 
 from quart import request
+
+PAPERTRAIL_HOST = "logs4.papertrailapp.com"
+PAPERTRAIL_PORT = 22206
 
 logger = logging.getLogger("bubbles_log")
 logger.setLevel(logging.INFO)
 
 # Log setup to rotate to new files every midnight
 log_name = f"logs/{datetime.now().strftime('%d-%m-%Y')}-app.log"
-log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",
+                               datefmt="%d-%m-%Y %H:%M:%S")
 
 file_handler = TimedRotatingFileHandler(log_name, when="midnight", interval=1)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(log_format)
 
+syslog_handler = SysLogHandler(address=(PAPERTRAIL_HOST, PAPERTRAIL_PORT))
+syslog_handler.setLevel(logging.INFO)
+syslog_handler.setFormatter(log_format)
+
 logger.addHandler(file_handler)
+logger.addHandler(syslog_handler)
 
 # For debugging on screen
 stream_handler = logging.StreamHandler()
