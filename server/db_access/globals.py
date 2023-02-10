9494015653d458a62392a3658f94c65452c9e951
br-1ght <fcw2k16@gmail.com>
2023-02-11 00:37:05 +0800

    Revert "whopper"
    This reverts commit 52b1cb99b5834787a1bc8c4fd147a65cecb2db84.

--- server/db_access/globals.py
+++ server/db_access/globals.py
@@ -1,13 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
-from os import getenv
-
-DB_USERNAME = getenv("DB_USERNAME") or "bubbles"
-DB_PASSWORD = getenv("DB_PASSWORD") or "bubbles"
-DB_ENDPOINT = getenv("DB_ENDPOINT") or "localhost"
 
 Base = declarative_base()
-connection_string = f"mysql+asyncmy://{DB_USERNAME}:{DB_PASSWORD}@{DB_ENDPOINT}:3306/bubbles"
+connection_string = "mysql+asyncmy://bubbles:bubbles@localhost:3306/bubbles"
 
 # echo is for debug
 engine = create_async_engine(connection_string, echo=False)
--- server/security_functions/virustotal.py
+++ server/security_functions/virustotal.py
@@ -1,11 +1,13 @@
 # scanning for hashes
 import aiohttp
+import asyncio
 import urllib.parse
-from os import getenv
 
 from models.error import VirusTotalError
 
-API_KEY = getenv("VT_API_KEY") or "ffe160af23a4fcb04ad18810d61be03b0bd0d7474dfc851dd986f1b41a4fe847"
+API_KEY = 'ffe160af23a4fcb04ad18810d61be03b0bd0d7474dfc851dd986f1b41a4fe847'
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 # step 1 upload file to vt api
@@ -115,3 +117,46 @@ async def get_url_report(url_id: str) -> dict | VirusTotalError:
             data = await response.json()
             score = data['data']['attributes']['last_analysis_stats']
             return score
+
+
+# Test using malicious file
+# print("Test 1: ")
+# score = asyncio.run(scan_file_hash('15e029c3834435150c76741e714540fcb799662db8cc2c61ba4ef192a781727b'))
+# if score > 0:
+#     print("Malicious file detected!")
+# else:
+#     print("File is safe!")
+
+# # Test using safe file
+# print("Test 2: ")
+# file_id = asyncio.run(upload_file(r".\server\security_functions\safe_file_2.txt"))
+# print("file id is = " + file_id)
+# file_hash = asyncio.run(get_file_analysis(file_id))
+# print("file hash = " + file_hash)
+# score = asyncio.run(scan_file_hash(file_hash))
+
+# if score > 0:
+#     print("Malicious file detected!")
+# else:
+#     print("File is safe!")
+
+
+# Test using malicious url
+# print("Test 3: ")
+# data_id = asyncio.run(upload_url('http://www.csm-testcenter.org/download/malicious/index.html'))
+# url_id = asyncio.run(get_url_analysis(data_id))
+# score = asyncio.run(get_url_report(url_id))
+# if score > 0:
+#     print("Malicious url detected!")
+# else:
+#     print("Url is safe!")
+
+# # Test using safe url
+# print("Test 4: ")
+# data_id = asyncio.run(upload_url('https://www.youtube.com'))
+# url_id = asyncio.run(get_url_analysis(data_id))
+# score = asyncio.run(get_url_report(url_id))
+# if score > 0:
+#     print("Malicious url detected!")
+# else:
+#     print("Url is safe!")
